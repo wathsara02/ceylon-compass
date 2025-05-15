ad5ddb14a3bbdf3403bc65330513f9dd0357d419
wathsara02 <wathsarakalharasilva@gmail.com>
2025-05-15 22:52:03 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
 })
